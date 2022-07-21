a8fedc269ff9fb13467c4131abd810c36e852f16
hiteshpattanayak-tw <hpattanayak@investvoyager.com>
2022-07-21 09:18:40 +0530

    Add DatabasePool interface

--- internal/pkg/db/db.go
+++ internal/pkg/db/db.go
@@ -1,1 +1,17 @@
 package db
+
+import "time"
+
+type DatabasePool interface {
+	SetMaxOpenConns(n int)
+	SetConnMaxLifetime(d time.Duration)
+	SetMaxIdleConns(n int)
+	SetConnMaxIdleTime(d time.Duration)
+}
+
+func ConfigureDatabasePool(pool DatabasePool, connection ConnectionPool) {
+	pool.SetMaxOpenConns(connection.MaxOpenConnections)
+	pool.SetConnMaxLifetime(time.Second * time.Duration(connection.MaxLifeTime))
+	pool.SetMaxIdleConns(connection.MaxIdleConnections)
+	pool.SetConnMaxIdleTime(time.Second * time.Duration(connection.MaxIdleTime))
+}
