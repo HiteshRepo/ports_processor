ad1ce71dd846c7aa0dfdbc07cfdafe91b7da37a8
hiteshpattanayak-tw <hpattanayak@investvoyager.com>
2022-07-21 09:39:50 +0530

    Implement UpsertPort method of PortRepository

--- internal/app/repository/port_repository.go
+++ internal/app/repository/port_repository.go
@@ -17,3 +17,12 @@ type portRepository struct {
 func ProvideTierConfigRepository(db *gorm.DB) PortRepository {
 	return &portRepository{portDb: db}
 }
+
+func (pr *portRepository) UpsertPort(ctx context.Context, port *model.Port) error {
+	err := pr.portDb.WithContext(ctx).Where(model.Port{Code: port.Code}).Save(port).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
